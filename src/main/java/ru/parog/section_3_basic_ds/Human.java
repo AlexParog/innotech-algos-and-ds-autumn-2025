d48a6c5c8fac971c62ad5f5bce1042964a8ebf22
Alex Parog <sbibik2000@mail.ru>
2025-09-28 18:05:08 +0300

    homework: hw №3 "Основные структуры данных"

---
+++ src/main/java/ru/parog/section_3_basic_ds/Human.java
@@ -0,0 +1,17 @@
+package ru.parog.section_3_basic_ds;
+
+public class Human {
+    private int socialRating;
+
+    public Human(int socialRating) {
+        this.socialRating = socialRating;
+    }
+
+    public int getSocialRating() {
+        return socialRating;
+    }
+
+    public void setSocialRating(int socialRating) {
+        this.socialRating = socialRating;
+    }
+}
